ed41ee4083022847590a375f6c942af4bb4c7f6b
Adwait S <67193262+Whoops-Aurora@users.noreply.github.com>
2023-12-13 09:39:20 +0530

    "Components added"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { HomeproductComponent } from './homeproduct/homeproduct.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    HomeproductComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/homeproduct/homeproduct.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homeproduct',
+  templateUrl: './homeproduct.component.html',
+  styleUrl: './homeproduct.component.css'
+})
+export class HomeproductComponent {
+
+}
